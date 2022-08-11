873721899efa457e3718dde3cb2df5f10493a499
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-11 12:41:47 -0400

    routes, create api

--- job_posting/routes/api.php
+++ job_posting/routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//create an api
+Route::get('/posts', function(){
+    return response()->json([
+        'posts' => [
+            'title' => 'post one'
+        ]
+    ]);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- job_posting/routes/web.php
+++ job_posting/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+//Passing id as param where id is a number
+Route::get("/hello/{id}", function($id){
+    return response('Post:' . $id);
+})->where('id', '[0-9]+');
+
+
+//URL = search/?name=opel&occ=developer
+Route::get('/search', function(Request $request){
+    return $request->name;
 });
